4f09997a297dcb2b89ef216fb0ef75f067666189
Kamila Matla-Tomczyk <ka1130@o2.pl>
2019-03-01 23:36:19 +0100

    Connect Firebase to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,30 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
+import { reactReduxFirebase } from "react-redux-firebase";
+import firebase from "firebase";
 import rootReducer from "redux/reducers";
 
-const store = createStore(rootReducer);
+// Firebase config
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// react-redux-firebase options
+const config = {
+  userProfile: "users", // firebase root where user profiles are stored
+  enableLogging: false // enable/disable Firebase's database logging
+};
+
+// Add redux Firebase to compose
+const createStoreWithFirebase = compose(reactReduxFirebase(firebase, config))(
+  createStore
+);
+
+// Create store with reducers and initial state
+const store = createStoreWithFirebase(rootReducer);
 
 export default store;
